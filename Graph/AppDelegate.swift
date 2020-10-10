7214875c8d121b90d151fb30a1d0088c126b2182
Frank McAuley <frank.mcauley@gmail.com>
2020-10-10 08:57:38 -0400

    FM - setting up project

---
+++ Graph/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Graph
+//
+//  Created by Frank McAuley on 10/10/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
